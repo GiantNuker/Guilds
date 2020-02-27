f47db43db695158dea4023bad20f21f5f09b490c
Indigo Amann <indigoa.coding@gmail.com>
2020-02-26 23:40:45 -0800

    Offline promotion

--- src/main/java/io/github/giantnuker/guilds/Guilds.java
+++ src/main/java/io/github/giantnuker/guilds/Guilds.java
@@ -7,6 +7,7 @@ import com.mojang.brigadier.suggestion.SuggestionProvider;
 import io.github.nyliummc.commands.BetterCommandContext;
 import io.github.nyliummc.commands.CommandFeedback;
 import io.github.nyliummc.commands.ServerCommandBuilder;
+import io.github.voidpointerdev.minecraft.offlineinfo.OfflineInfo;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.registry.CommandRegistry;
 import net.minecraft.command.arguments.ColorArgumentType;
@@ -42,13 +43,6 @@ public class Guilds implements ModInitializer {
 					{"leave", "Leave your guild"},
 					{"chat", "Send a message in private guild chat"},
 					{"help", "Show this message"}
-	};
-	private static final String[][] rankHelpArray = new String[][] {
-					{"create", "rank", "priority", "Create a new rank"},
-					{"prioritize", "rank", "priority", "Change a rank's priority"},
-					{"remove", "rank", "Remove a rank"},
-					{"set", "player", "rank", "Change a guild member's rank"},
-					{"color", "rank", "color", "Change the color of a rank"}
 	};
 	public static Config CONFIG = new Config();
 	public static GuildManager GM = new GuildManager();
@@ -118,20 +112,6 @@ public class Guilds implements ModInitializer {
 			context.getSource().sendFeedback(new LiteralText(fullcommand + " - ").setStyle(new Style().setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, command)).setColor(Formatting.GRAY)).append(new LiteralText(cmd[cmd.length - 1]).formatted(Formatting.GREEN)), false);
 		}
 		context.getSource().sendFeedback(new LiteralText("-----------------------------").formatted(Formatting.DARK_GREEN), false);
-	}
-
-	private static void rankHelp(BetterCommandContext<ServerCommandSource> context, CommandFeedback feedback) {
-		context.getSource().sendFeedback(new LiteralText("---------- Guild Help: Ranks ----------").formatted(Formatting.DARK_GREEN), false);
-		for (String[] cmd : rankHelpArray) {
-			String command = "/guild ranks " + cmd[0];
-			String fullcommand = command;
-			for (int i = 1; i < cmd.length - 1; i++) {
-				fullcommand += " <" + cmd[i] + ">";
-			}
-			context.getSource().sendFeedback(new LiteralText(fullcommand + " - ").setStyle(new Style().setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, command)).setColor(Formatting.GRAY)).append(new LiteralText(cmd[cmd.length - 1]).formatted(Formatting.GREEN)), false);
-		}
-		context.getSource().sendFeedback(new LiteralText("Note that priorities are sorted from lowest to highest, lowest being the best").formatted(Formatting.GRAY), false);
-		context.getSource().sendFeedback(new LiteralText("------------------------------------").formatted(Formatting.DARK_GREEN), false);
 	}
 
 	private static void acceptInvite(BetterCommandContext<ServerCommandSource> context, CommandFeedback feedback) throws CommandSyntaxException {
@@ -219,10 +199,17 @@ public class Guilds implements ModInitializer {
 
 	private static void promote(BetterCommandContext<ServerCommandSource> context, CommandFeedback feedback) throws CommandSyntaxException {
 		if (doOwnerCheck(context)) {
-			PlayerEntity player = EntityArgumentType.getPlayer(context, "player");
-			GM.getGuild(uuid(context)).setOwner(player.getGameProfile().getId());
-			player.sendMessage(new LiteralText("You have been promoted to leader of your guild").formatted(Formatting.GREEN));
-			context.getSource().sendFeedback(new LiteralText("You have promoted ").formatted(Formatting.GREEN).append(Team.modifyText(player.getScoreboardTeam(), player.getName())).append(new LiteralText(" to guild leader").formatted(Formatting.GREEN)), false);
+			UUID player = OfflineInfo.getUUID(context, "player");
+			if (GM.getGuild(player) == GM.getGuild(uuid(context))) {
+				GM.getGuild(uuid(context)).setOwner(player);
+				PlayerEntity pe = context.getSource().getMinecraftServer().getPlayerManager().getPlayer(player);
+				if (pe != null) {
+					pe.sendMessage(new LiteralText("You have been promoted to leader of your guild").formatted(Formatting.GREEN));
+				}
+				context.getSource().sendFeedback(new LiteralText("You have promoted ").formatted(Formatting.GREEN).append(OfflineInfo.getNameById(context.getSource().getMinecraftServer().getUserCache(), player)).append(new LiteralText(" to guild leader").formatted(Formatting.GREEN)), false);
+			} else {
+				context.getSource().sendFeedback(new LiteralText(String.format("%s is not a member of your guild", StringArgumentType.getString(context, "player"))).formatted(Formatting.RED), false);
+			}
 		}
 	}
 
@@ -255,16 +242,8 @@ public class Guilds implements ModInitializer {
 						.literal("ask").up()
 						.literal("close").up()
 						.root()
-						.defineArgument("player", EntityArgumentType.player()).definitionDone()
-						.literal("ranks").executes(Guilds::rankHelp)
-						.literal("create").argument("rank", StringArgumentType.word()).up("ranks")
-						.defineArgument("rank", StringArgumentType.word()).definitionDone()
-						.literal("remove").predefinedArgument("rank").up("ranks")
-						.literal("set").predefinedArgument("player").predefinedArgument("rank").up("ranks")
-						.literal("color").predefinedArgument("rank").argument("color", ColorArgumentType.color()).up("ranks")
-						.literal("help").executes(Guilds::rankHelp).up()
-						.root()
-						.literal("invite").predefinedArgument("player").executes(Guilds::invite).root()
+						.literal("invite").argument("player", EntityArgumentType.player()).executes(Guilds::invite).root()
+						.defineArgument("player", StringArgumentType.word()).suggest(OfflineInfo.ONLINE_PROVIDER).definitionDone()
 						.literal("promote").predefinedArgument("player").executes(Guilds::promote).root()
 						.literal("invites").executes(Guilds::listInvites).root()
 						.defineArgument("invite", StringArgumentType.word()).suggest(INVITES_PROVIDER).definitionDone()
